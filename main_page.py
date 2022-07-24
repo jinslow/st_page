0a4cf88a07c0c33c732888c5a432dd4ca05790f3
slow-jin <105343318+slow-jin@users.noreply.github.com>
2022-07-25 01:25:17 +0900

    Update main_page.py

--- main_page.py
+++ main_page.py
@@ -6,13 +6,9 @@ st.markdown('## 쇼핑몰 리뷰 평점 분류')
 st.markdown('### 1PICK (1팀 - 자연어 처리)')
     
 path = ""
-image1 = Image.open(f'{path}NLP.jpg')
+image1 = Image.open(f'{path}RNN.png')
 image2 = Image.open(f'{path}pytorch.png')
-image3 = Image.open(f'{path}RNN.png')
-
-with st.container():
-    st.image(image1, width=400)
     
 with st.container():
+    st.image(image1, width=400)
     st.image(image2, width=400)
-    st.image(image3, width=400)
